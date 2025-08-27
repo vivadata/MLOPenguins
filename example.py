26166e6115c4e043afd8d29249ec0b9bbe62f7e4
Aloys Bernard <aloys.bernard@artefact.com>
2025-08-27 12:51:18 +0200

    Le flow

---
+++ example.py
@@ -0,0 +1,60 @@
+from prefect import flow, task
+from prefect.logging import loggers
+import loguru 
+import time
+
+from loguru import logger
+
+@task
+def load_data() :
+    logger = loggers.get_run_logger()
+    logger.info("Is this correct ?")
+    loggers.print_as_log("✅ Data Loaded")
+    time.sleep(2)
+    return 1
+
+@task 
+def preproc(X):
+    loguru.logger.info("✅ Data preprocessed")
+    time.sleep(2)
+    return 1
+
+@task 
+def get_X_y(data):
+    return "X_train","y_train","X_test","y_test"
+
+@task 
+def train(X_train,y_train):
+    time.sleep(3)
+    return
+    
+@task 
+def evaluate(model,X_test,y_test):
+    time.sleep(3)
+    return 1
+    
+@task 
+def notify(trained,score):
+    logger = loggers.get_run_logger()
+    logger.info("✅ Model Trained")
+    
+@flow
+def main(): 
+    data = load_data()
+    X_train,y_train,X_test,y_test = get_X_y(data)
+    X_train_preproc = preproc(X_train)
+    X_test_preproc = preproc(X_test)
+    model = train(X_train,y_train)
+    score = evaluate(model,X_test,y_test)
+    notify(model,score)
+
+from datetime import timedelta, datetime
+from prefect.schedules import Interval
+if __name__ == "__main__" : 
+    main(
+        # name = "my-little-deployment",
+        # cron =  "*/5 * * * *",
+        # schedule= Interval(timedelta(minutes=5)
+        #                    , anchor_date=datetime(2025,8,27,13,0,0)
+        #                    , timezone="Europe/Paris")
+    )
--- main.py
+++ main.py
@@ -1,21 +1,52 @@
-
+from prefect import get_run_logger
+from prefect import flow,task
 import loguru
 from penguins.data import load_data, preprocess_data, clean_data, get_X_y
 from penguins.model import instantiate_model,train_model, evaluate_model, save_model
 
 logger = loguru.logger 
 
+@task
+def load_data_task():
+    return load_data()
+    
+@task
+def clean_data_task(*args,**kwargs):
+    return clean_data(*args,**kwargs)
+
+@task
+def preprocess_data_task(*args,**kwargs):
+    return preprocess_data(*args,**kwargs)
+
+@task
+def get_X_y_task(*args,**kwargs):
+    return get_X_y(*args,**kwargs)
+
+@task
+def instantiate_model_task(*args,**kwargs):
+    return instantiate_model(*args,**kwargs)
+
+@task
+def train_model_task(model, X, y):
+    return train_model(model, X, y)
+
+@task
+def evaluate_model_task(model, X, y):
+    return evaluate_model(model, X, y)
+
+@flow
 def train():
-    data = load_data()
-    cleaned_data = clean_data(data)
-    X_train, X_test, y_train, y_test = get_X_y(cleaned_data)
-    X_train_preproc = preprocess_data(X_train, fit=True)
-    X_test_preproc = preprocess_data(X_test, fit=False)
+    logger = get_run_logger()
+    data = load_data_task()
+    cleaned_data = clean_data_task(data)
+    X_train, X_test, y_train, y_test = get_X_y_task(cleaned_data)
+    X_train_preproc = preprocess_data_task(X_train, fit=True)
+    X_test_preproc = preprocess_data_task(X_test, fit=False)
     logger.info("✅ Data preprocessing complete")  
-    model = instantiate_model(fit=True)
-    model = train_model(model,X_train_preproc, y_train)
+    model = instantiate_model_task(fit=True)
+    model = train_model_task(model,X_train_preproc, y_train)
     logger.info("✅ Model training complete")
-    evaluate_model(model, X_test_preproc, y_test)
+    evaluate_model_task(model, X_test_preproc, y_test)
 
 if __name__ == "__main__":
     train()
